b7f8e4b88225707a5b437188295b7c8e7336aab8
ryomak <ryooomaaa0413@gmail.com>
2024-10-14 11:14:55 +0900

    大改造

--- example/simple/main.go
+++ example/simple/main.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 	err := p5go.Run("main",
-		p5go.Setup(func(p *p5go.P5Instance) {
+		p5go.Setup(func(p *p5go.Canvas) {
 			p.CreateCanvas(400, 400)
 			p.Background(128, 200, 128)
 		}),
-		p5go.Draw(func(p *p5go.P5Instance) {
+		p5go.Draw(func(p *p5go.Canvas) {
 			p.Fill(0)
 			p.Ellipse(200, 200, 50, 50)
 			p.Text("Hello, p5go", 50, 20)
